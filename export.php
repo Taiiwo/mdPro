32ce5ca017280db3ceea8f7fd81f22a312e74831
Taiiwo Llort <tylerzacc@gmail.com>
2014-12-05 10:51:23 -0500

    Save the html as a temp file, then upload it to the API

--- export.php
+++ export.php
@@ -3,32 +3,30 @@ $html = 	$_REQUEST['html'];
 $format = 	$_REQUEST['type'];
 
 // if not $format in ['pdf','docx','odt','xml','html','markdown']:
-if (array_keys(array(['pdf','docx','odt','xml','html','markdown']), $format).count == 0){
+if (!in_array($format, array('pdf','docx','odt','xml','html','markdown'))){
 	die("Invalid Format");
 }
+//Save string into temp file
+$file = tempnam(sys_get_temp_dir(), 'POST');
+file_put_contents($file, $html);
 
 //set POST variables
 $url = 'http://c.docverter.com/convert';
 $fields = array('from' => 'html',
     'to' => $format,
-    'input_files[]' => array($html)
-    );
-
+    'input_files[]' => '@'.$file
+);
 //open connection
 $ch = curl_init();
-
-//set options 
+//set options
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: multipart/form-data"));
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $fields); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //needed so that the $result=curl_exec() output is the file and isn't just true/false
-
 //execute post
 $result = curl_exec($ch);
-
 //close connection
 curl_close($ch);
-
 //write to file
 header("Content-disposition: attachment;filename=export.$format");
 echo $result;
